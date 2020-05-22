a1ebdb0c6d65a22ac61b66a6ac06ef5ebc1d6a39
Mohit Arora <mohit.arora1902@gmail.com>
2020-05-22 13:25:32 +0530

    Fixed Event Service Provider

--- src/WebSockets/WebSocketHandler.php
+++ src/WebSockets/WebSocketHandler.php
@@ -4,6 +4,7 @@ namespace BeyondCode\LaravelWebSockets\WebSockets;
 
 use BeyondCode\LaravelWebSockets\Apps\App;
 use BeyondCode\LaravelWebSockets\Dashboard\DashboardLogger;
+use BeyondCode\LaravelWebSockets\Events\WebsocketOnOpen;
 use BeyondCode\LaravelWebSockets\Facades\StatisticsLogger;
 use BeyondCode\LaravelWebSockets\QueryParameters;
 use BeyondCode\LaravelWebSockets\WebSockets\Channels\ChannelManager;
@@ -33,6 +34,59 @@ class WebSocketHandler implements MessageComponentInterface
             ->limitConcurrentConnections($connection)
             ->generateSocketId($connection)
             ->establishConnection($connection);
+
+        event(new WebsocketOnOpen($connection));
+    }
+
+    protected function establishConnection(ConnectionInterface $connection)
+    {
+        $connection->send(json_encode([
+            'event' => 'pusher:connection_established',
+            'data' => json_encode([
+                'socket_id' => $connection->socketId,
+                'activity_timeout' => 30,
+            ]),
+        ]));
+
+        DashboardLogger::connection($connection);
+
+        StatisticsLogger::connection($connection);
+
+        return $this;
+    }
+
+    protected function generateSocketId(ConnectionInterface $connection)
+    {
+        $socketId = sprintf('%d.%d', random_int(1, 1000000000), random_int(1, 1000000000));
+
+        $connection->socketId = $socketId;
+
+        return $this;
+    }
+
+    protected function limitConcurrentConnections(ConnectionInterface $connection)
+    {
+        if (!is_null($capacity = $connection->app->capacity)) {
+            $connectionsCount = $this->channelManager->getConnectionCount($connection->app->id);
+            if ($connectionsCount >= $capacity) {
+                throw new ConnectionsOverCapacity();
+            }
+        }
+
+        return $this;
+    }
+
+    protected function verifyAppKey(ConnectionInterface $connection)
+    {
+        $appKey = QueryParameters::create($connection->httpRequest)->get('appKey');
+
+        if (!$app = App::findByKey($appKey)) {
+            throw new UnknownAppKey($appKey);
+        }
+
+        $connection->app = $app;
+
+        return $this;
     }
 
     public function onMessage(ConnectionInterface $connection, MessageInterface $message)
@@ -60,56 +114,5 @@ class WebSocketHandler implements MessageComponentInterface
                 $exception->getPayload()
             ));
         }
-    }
-
-    protected function verifyAppKey(ConnectionInterface $connection)
-    {
-        $appKey = QueryParameters::create($connection->httpRequest)->get('appKey');
-
-        if (! $app = App::findByKey($appKey)) {
-            throw new UnknownAppKey($appKey);
-        }
-
-        $connection->app = $app;
-
-        return $this;
-    }
-
-    protected function limitConcurrentConnections(ConnectionInterface $connection)
-    {
-        if (! is_null($capacity = $connection->app->capacity)) {
-            $connectionsCount = $this->channelManager->getConnectionCount($connection->app->id);
-            if ($connectionsCount >= $capacity) {
-                throw new ConnectionsOverCapacity();
-            }
-        }
-
-        return $this;
-    }
-
-    protected function generateSocketId(ConnectionInterface $connection)
-    {
-        $socketId = sprintf('%d.%d', random_int(1, 1000000000), random_int(1, 1000000000));
-
-        $connection->socketId = $socketId;
-
-        return $this;
-    }
-
-    protected function establishConnection(ConnectionInterface $connection)
-    {
-        $connection->send(json_encode([
-            'event' => 'pusher:connection_established',
-            'data' => json_encode([
-                'socket_id' => $connection->socketId,
-                'activity_timeout' => 30,
-            ]),
-        ]));
-
-        DashboardLogger::connection($connection);
-
-        StatisticsLogger::connection($connection);
-
-        return $this;
     }
 }
